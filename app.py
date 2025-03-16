15173a9d6a9a2e424f5d6089ac286eaaae37308e
Mekkilangelo <mekki.abdessatar@gmail.com>
2025-03-16 11:51:41 +0100

    test

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def home():
     return "<h1>Hello World </h1>"
 
 if __name__ =='__main__':
-    app.run(debug=True, host='localhost', port= 5000)
+    app.run(debug=True, host='localhost', port= 6000)
